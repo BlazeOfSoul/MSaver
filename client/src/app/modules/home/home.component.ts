e269bf705c5c9138fe015d8dea82a42a543e3225
BlazeOfSoul <rostislavkolb@gmail.com>
2025-04-16 21:38:10 +0300

    add: summary-cards

--- client/src/app/modules/home/home.component.ts
+++ client/src/app/modules/home/home.component.ts
@@ -1,18 +1,21 @@
 import { Component } from '@angular/core';
-import { Router } from '@angular/router';
 import { ButtonModule } from 'primeng/button';
+import { SummaryCardComponent } from '../../shared/components/summary-card/summary-card.component';
 
 @Component({
     selector: 'app-home',
     standalone: true,
-    imports: [ButtonModule],
+    imports: [ButtonModule, SummaryCardComponent],
     templateUrl: './home.component.html',
     styleUrl: './home.component.scss',
 })
 export class HomeComponent {
-    constructor(private router: Router) {}
+    user = {
+        username: 'Юзер',
+    };
 
-    goFaq() {
-        this.router.navigate(['/faq']);
-    }
+    monthName = 'апрель';
+    income = 2500;
+    balance = 1000;
+    expenses = 1500;
 }
