e806bfe166ecc80652a39d3e8d8907754d430b95
BlazeOfSoul <rostislavkolb@gmail.com>
2025-06-25 13:36:51 +0300

    update: design

--- client/src/app/shared/components/exchange-rates/exchange-rates.component.ts
+++ client/src/app/shared/components/exchange-rates/exchange-rates.component.ts
@@ -1,12 +1,13 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
+import { CardModule } from 'primeng/card';
 import { TableModule } from 'primeng/table';
 import { Rate } from '../../../core/models/exchange-rate/rate.model';
 
 @Component({
     selector: 'app-exchange-rates',
     standalone: true,
-    imports: [CommonModule, TableModule],
+    imports: [CommonModule, TableModule, CardModule],
     templateUrl: './exchange-rates.component.html',
     styleUrl: './exchange-rates.component.scss',
 })
--- client/src/app/shared/components/summary-card/summary-card.component.ts
+++ client/src/app/shared/components/summary-card/summary-card.component.ts
@@ -1,11 +1,10 @@
-import { NgClass } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { CardModule } from 'primeng/card';
 
 @Component({
     selector: 'app-summary-card',
     standalone: true,
-    imports: [CardModule, NgClass],
+    imports: [CardModule],
     templateUrl: './summary-card.component.html',
     styleUrls: ['./summary-card.component.scss'],
 })
@@ -13,6 +12,5 @@ export class SummaryCardComponent {
     @Input() title!: string;
     @Input() value!: number | string;
     @Input() backgroundColor: string = 'rgba(255, 255, 255, 0.8)';
-    @Input() scale: number = 1;
     @Input() textColor: string = '#000000';
 }
