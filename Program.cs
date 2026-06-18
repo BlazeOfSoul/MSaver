c35b0b43552202f1f8b73c0e1037153867962442
Rostislav Kolb <rostislavkolb@gmail.com>
2026-06-18 20:53:30 +0300

    health fix

--- Program.cs
+++ Program.cs
@@ -1,11 +1,17 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+
 using MSaver.Api.Configuration;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddApiServices(builder.Configuration);
+builder.Services.AddHealthChecks();
 
 var app = builder.Build();
 
 app.UseApiPipeline();
+
+app.MapHealthChecks("/health/ready");
+app.MapHealthChecks("/health");
 
 app.Run();
